3cf83700c664632171a4c6313305e7202ccd0bae
Leandro Silva <lsilva@tremorvideo.com>
2017-05-17 23:00:01 -0400

    - Update inline docs. - Rename 'isValid' method to 'isValidAddress.' - Change jsdoc theme. - Update LICENSE. - Update package.json. - Include distribution versions.

--- src/EmailValidatorPro.js
+++ src/EmailValidatorPro.js
@@ -1,114 +1,92 @@
 /**
- * @author Leandro Silva | Grafluxe, 2012+2017
- * @license MIT license
- */
+ * @author Leandro Silva
+ * @copyright 2012, 2017 Leandro Silva (http://grafluxe.com)
+ * @license MIT
+ *
+ * @classdesc
+ * Validates an email address against most RFC standards.
+ *
+ * <p>
+ *   <b>Valid email addresses that are supported:</b>
+ *   <ul>
+ *     <li>niceandsimple@example.com</li>
+ *     <li>very.common@example.com</li>
+ *     <li>a.little.lengthy.but.fine@dept.example.com</li>
+ *     <li>disposable.style.email.with+symbol@example.com</li>
+ *     <li>other.email-with-dash@example.com</li>
+ *     <li>"much.more unusual"@example.com</li>
+ *     <li>"very.unusual.@.unusual.com"@example.com</li>
+ *     <li>#!$%&\'*+-/=?^_\`{}|~@example.org</li>
+ *     <li>" "@example.org</li>
+ *     <li>üñîçøðé@üñîçøðé.com</li>
+ *     <li>admin@mailserver1</li>
+ *     <li>user@tt</li>
+ *     <li>"hi"@example.com</li>
+ *     <li>jsmith@[192.168.2.1]</li>
+ *     <li>jsmith@[IPv6:2001:db8::1]</li>
+ *     <li>email@123.123.123.123</li>
+ *     <li>"()<>[]:,;@\\\"!#$%&\'*+-/=?^_\`{}| ~.a"@example.org</li>
+ *     <li>abc."defghi".xyz@example.com</li>
+ *     <li>_______@example.com</li>
+ *     <li>very.unusual."@".unusual.com@example.com</li>
+ *     <li>john.smith(comment)@example.com</li>
+ *     <li>(comment)john.smith@example.com</li>
+ *     <li>"()<>[]:,;@\\\"!#$%&\'*+-/=?^_\`{}| ~.a"(comment)@example.org</li>
+ *   </ul>
+ *
+ *   <b>Valid (but rare) email addresses that are not supported:</b>
+ *   <ul>
+ *     <li>"very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com</li>
+ *     <li>"()<>[]:,;@\"!#$%&\'*+-/=?^_\`{}| ~.a"@example.org</li>
+ *     <li>abc."d\"efghi".xyz@example.com</li>
+ *     <li>much."more\ unusual"@example.com</li>
+ *   </ul>
+ * </p>
+*/
 
 //jshint esversion:6, node:true
 
-/**
- * Validates an email address against most RFC standards.
- * See the email address <a href="http://en.wikipedia.org/wiki/E-mail_address#Local_part" target="_blank">wiki</a>.
- *
- * @example
- * Valid email addresses that are supported:
- * <pre>
- *  niceandsimple@example.com
- *  niceandsimple@example123-4.com
- *  niceandsimple@ex.example.com
- *  niceandsimple@ex.xx.xx.example.com
- *  very.common@example.com
- *  a.little.lengthy.but.fine@dept.example.com
- *  disposable.style.email.with+symbol@example.com
- *  other.email-with-dash@example.com
- *  "much.more unusual"@example.com
- *  "very.unusual.@.unusual.com"@example.com
- *  #!$%&\'*+-/=?^_\`{}|~@example.org
- *  " "@example.org
- *  üñîçøðé@example.com
- *  üñîçøðé@üñîçøðé.com
- *  admin@mailserver1
- *  user@tt
- *  "hi"@example.com
- *  jsmith@[192.168.2.1]
- *  jsmith@[IPv6:2001:db8::1]
- *  postbox@com
- *  !#$%&\'*+-/=?^_\`{}|~@example.org
- *  "()<>[]:,;@\\\"!#$%&\'*+-/=?^_\`{}| ~.a"@example.org
- *  "()<>[]:,;@\\!#$%&\'*+-/=?^_\`{}| ~.a"@example.org
- *  abc."defghi".xyz@example.com
- *  abc."d\\efghi".xyz@example.com
- *  "abcdefghixyz"@example.com
- *  email@example.com
- *  firstname.lastname@example.com
- *  email@subdomain.example.com
- *  firstname+lastname@example.com
- *  email@123.123.123.123
- *  email@[123.123.123.123]
- *  "email"@example.com
- *  1234567890@example.com
- *  email@example-one.com
- *  _______@example.com
- *  email@example.co.jp
- *  firstname-lastname@example.com
- *  very.unusual."@".unusual.com@example.com
- *  john.smith(comment)@example.com
- *  (comment)john.smith@example.com
- *  "()<>[]:,;@\\\"!#$%&\'*+-/=?^_\`{}| ~.a"(comment)@example.org
- * </pre>
- *
- * Valid email addresses that are not supported:
- * <pre>
- *  "very.(),:;<>[]\".VERY.\"very@\\ \"very\".unusual"@strange.example.com
- *  "()<>[]:,;@\"!#$%&\'*+-/=?^_\`{}| ~.a"@example.org
- *  abc."d\"efghi".xyz@example.com
- *  much."more\ unusual"@example.com
- * </pre>
-*/
 class EmailValidatorPro {
   /**
    * Checks whether an email address is valid.
    * @param   {String}  address An email address.
    * @returns {Boolean}
    */
-  isValid(address) {
-    let whitelisted = this._whitelist(address),
-        blacklisted = this._blacklist(address),
-        tooLong = this._tooLong(address),
-        validComments = this._comments(address);
-
-    return (whitelisted && !blacklisted && !tooLong) || (!blacklisted && validComments);
+  isValidAddress(address) {
+    return (this._whitelist(address) && !this._blacklist(address) && !this._tooLong(address));
   }
 
   /**
-   * [[Description]]
-   * @param   {String} address [[Description]]
-   * @returns {Object} [[Description]]
+   * Gets the two parts of an email address (local and domain). This method does not check your email validity.
+   * @param   {String} address An email address.
+   * @example
+   * let evp = new EmailValidatorPro(),
+   *     parts = evp.getParts("contact@email.com");
+   *
+   * console.log(parts); //{local: "contact", domain: "email.com"}
+   * @returns {Object}
    */
   getParts(address) {
-    let mat = address.match(/[^@]+$/);
+    let domainMatch = address.match(/[^@]*$/);
 
     return {
-      local: address.substr(0, mat.index - 1),
-      domain: mat[0]
+      local: address.substr(0, domainMatch.index - 1) || "",
+      domain: domainMatch[0] || ""
     };
   }
 
-  _whitelist(addr) {
-    return /^(?=\s)|^(?:(?!.+\.{2,})(?!\.)(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+[^\."]@|^"(?:[\w.!#$%&'*+\-\/=?\^`{|} ~(),:;<>@\[\]]|[^\x00-\x7F])+"@|(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+\."(?:[\w.!#$%&'*+\-\/=?\^`{|} ~"(),:;<>\\@\[\]]|[^\x00-\x7F])+"\.(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+@)(?!-)(?!.*-\.)(?:[a-zA-Z0-9-.]|[^\x00-\x7F])+$|^".+"@.+|.+@\[(?:\w+\.|\w+:){3}.+\]/.test(addr);
+  _whitelist(address) {
+    return /^(?=\s)|^(?:(?!.+\.{2,})(?!\.)(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+[^\."]@|^"(?:[\w.!#$%&'*+\-\/=?\^`{|} ~(),:;<>@\[\]]|[^\x00-\x7F])+"@|(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+\."(?:[\w.!#$%&'*+\-\/=?\^`{|} ~"(),:;<>\\@\[\]]|[^\x00-\x7F])+"\.(?:[\w.!#$%&'*+\-\/=?\^`{|} ~]|[^\x00-\x7F])+@)(?!-)(?!.*-\.)(?:[a-zA-Z0-9-.]|[^\x00-\x7F])+$|^".+"@.+|.+@\[(?:\w+\.|\w+:){3}.+\]|^\(.+\)|\(.+\)@/.test(address);
   }
 
-  _blacklist(addr) {
-    return /^@|^\s|@\[?(?=\d).*\d{4,}|"(?!.+\\").+".+"/.test(addr);
+  _blacklist(address) {
+    return /^@|^\s|@\[?(?=\d).*\d{4,}|"(?!.+\\").+".+"|.\(.+\)@(?=.+@)/.test(address);
   }
 
-  _tooLong(addr) {
-    let pts = this.getParts(addr);
+  _tooLong(address) {
+    let pts = this.getParts(address);
 
-    return (pts.domain.length >= 253 || pts.local.length >=64 || addr.length >= 254);
-  }
-
-  _comments(addr) {
-    return /^\(.+\)|\(.+\)@/.test(addr.replace(/".+"/g, "-"));
+    return (pts.domain.length >= 253 || pts.local.length >= 64 || address.length >= 254);
   }
 
 }
